30bfe52680e6efbf80f9fa2c64041ceab59e7b1b
Boo4S <122552410+Boo4S@users.noreply.github.com>
2023-05-08 20:23:31 +0200

    Delete Projet Newsletter directory

--- Projet Newsletter/scraping/doublons.py
+++
@@ -1,8 +0,0 @@
-def trouver_doublons(articles, article):
-    """
-    Vérifie si un article est un doublon en se basant sur l'URL et le titre de l'article.
-    """
-    for a in articles:
-        if a["url"] == article["url"] or a["titre"] == article["titre"]:
-            return True
-    return False
